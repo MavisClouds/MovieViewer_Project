6154240b0d0ef72525f9ba2d22e5bbcd78a92caf
feronikanm <firaferonika27@gmail.com>
2020-05-05 11:47:35 +0700

    update #3

--- app/src/main/java/com/example/movieviewer_project/DetailActivity.java
+++ app/src/main/java/com/example/movieviewer_project/DetailActivity.java
@@ -3,9 +3,12 @@ package com.example.movieviewer_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.frogobox.frogothemoviedbapi.data.model.TrendingMovie;
+import com.frogobox.frogothemoviedbapi.util.MovieUrl;
 import com.google.gson.Gson;
 
 public class DetailActivity extends AppCompatActivity {
@@ -18,9 +21,29 @@ public class DetailActivity extends AppCompatActivity {
         String extra = getIntent().getStringExtra("EXTRA_TRENDING_MOVIE");
         TrendingMovie trendingMovie = new Gson().fromJson(extra, TrendingMovie.class);
 
-        TextView text = findViewById(R.id.textView);
-        text.setText(trendingMovie.getOverview());
+        ImageView posterToolbarDetail = findViewById(R.id.img_photo_toolbar);
+        Glide.with(this).load(MovieUrl.BASE_URL_IMAGE_ORIGNAL+trendingMovie.getBackdrop_path()).into(posterToolbarDetail);
+
+        TextView releaseDateDetail = findViewById(R.id.tv_release_date_detail);
+        releaseDateDetail.setText(trendingMovie.getTitle());
+
+        TextView rateDetail = findViewById(R.id.tv_rate_detail);
+        rateDetail.setText(trendingMovie.getTitle());
+
+        TextView popularityDetail = findViewById(R.id.tv_popularity_detail);
+        popularityDetail.setText(trendingMovie.getTitle());
+
+        TextView titleDetail = findViewById(R.id.tv_title_detail);
+        titleDetail.setText(trendingMovie.getTitle());
+
+        TextView descriptionDetail = findViewById(R.id.tv_description_detail);
+        descriptionDetail.setText(trendingMovie.getOverview());
+
+        ImageView posterDetail = findViewById(R.id.iv_poster_detail);
+        Glide.with(this).load(MovieUrl.BASE_URL_IMAGE_ORIGNAL+trendingMovie.getPoster_path()).into(posterDetail);
 
 
     }
+
+
 }
